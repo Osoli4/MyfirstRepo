d6abe02a7d37fc78dc654785f848535ac074b2d8
Osoli4 <125508684+Osoli4@users.noreply.github.com>
2023-02-21 20:03:05 -0500

    Update GitPracticeTwo.java
    push practice

--- src/test/GitPracticeTwo.java
+++ src/test/GitPracticeTwo.java
@@ -4,7 +4,9 @@ public class GitPracticeTwo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+system.out.println("push ");
+		system.out.println("for pull ");
+		
 	}
 
 }
